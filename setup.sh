56c618df9e8dc73676504b7aac0a31fad00dfdae
Yagodica <ivantey228@gmail.com>
2022-05-21 13:25:50 +0300

    f2

--- setup.sh
+++ setup.sh
@@ -3,18 +3,20 @@
 set -e
 
 sudo apt-get update && sudo apt-get upgrade -y
-
-sudo apt-get install -y python3-pip \
+sudo apt-get install -y python3-gi python3-gi-cairo python3-gi-dbg libdbus-1-dev
+sudo apt-get install -y curl \
+						wget \
+						python3-pip \
 						python3 \
 						build-essential \
 						libssl-dev \
 						libffi-dev \
 						python3-dev \
 						python3-venv \
-						python-gobject \
-						python-dbus \
+						python3-dbus \
+						dbus \
 						neofetch
-						
+sudo pip3 install autokey
 						
 git clone https://github.com/nagygergo/jetbrains-toolbox-install.git
 
